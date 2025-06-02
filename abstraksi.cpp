a171f8e8e1af6d11d86026e6b9bf725b5294fc6c
rachelnova18 <rachelnv26@gmail.com>
2025-06-02 18:58:19 +0700

    Definisikan kelas AbstraksiKlas dengan atribut dan deklarasi method

--- abstraksi.cpp
+++ abstraksi.cpp
@@ -1,3 +1,13 @@
 #include <iostream>
 using namespace std;
 
+class AbstraksiKlas
+{
+private:
+    string x, y;
+
+public:
+    // method untuk mengisi nilai x dan y
+    //  private member
+    void setXY(string a, string b)
+ 
