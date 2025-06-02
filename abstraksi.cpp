8650af59d3c1cebae64f4507a5a57b7d0580608d
rachelnova18 <rachelnv26@gmail.com>
2025-06-02 18:58:49 +0700

    Implementasi method setXY dan display

--- abstraksi.cpp
+++ abstraksi.cpp
@@ -10,4 +10,15 @@ public:
     // method untuk mengisi nilai x dan y
     //  private member
     void setXY(string a, string b)
- 
+    {
+        x = a;
+        y = b;
+    }
+    // menampilkan nilai
+    void display()
+    {
+        cout << "x = " << x << endl;
+        cout << "y = " << y << endl;
+    }
+};
+
