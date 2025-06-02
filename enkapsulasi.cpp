3f60d5baf624faa11ccceba9a77307742c7563b6
rachelnova18 <rachelnv26@gmail.com>
2025-06-02 19:06:37 +0700

    fungsi utama

--- enkapsulasi.cpp
+++ enkapsulasi.cpp
@@ -17,3 +17,19 @@ public:
     }
 };
 
+int main()
+{
+    remoteLampu lampuRumah;
+
+    lampuRumah.setSaklarNo(0, "Lamppu Teras Rumah");
+    lampuRumah.setSaklarNo(1, "Lampu Ruang Tamu");
+    lampuRumah.setSaklarNo(2, "Lampu Kamar Tidur");
+    lampuRumah.setSaklarNo(3, "Lampu Dapur");
+
+    cout << lampuRumah.getSaklarNo(0) << endl; // Output: Lampu Teras Rumah
+    cout << lampuRumah.getSaklarNo(1) << endl; // Output: Lampu Ruang Tamu
+    cout << lampuRumah.getSaklarNo(2) << endl; // Output: Lampu Kamar Tidur
+    cout << lampuRumah.getSaklarNo(3) << endl; // Output: Lampu Dapur
+
+    return 0;
+}
